81b47e269bab224a8411713072edd82300bb4adf
strager <strager.nds@gmail.com>
2010-11-04 17:07:37 -0400

    Initial commit from 1.0.4.

---
+++ Sample Project/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace Samples
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+
+  }
+}
---
+++ WindowsFormsSample/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using Hardcodet.Wpf.TaskbarNotification;
+using Samples;
+using WindowsFormsSample.Properties;
+
+namespace WindowsFormsSample
+{
+  public partial class Form1 : Form
+  {
+    private TaskbarIcon notifyIcon;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+      base.OnLoad(e);
+      notifyIcon = new TaskbarIcon();
+      notifyIcon.Icon = Resources.Led;
+      notifyIcon.ToolTipText = "Left-click to open popup";
+      notifyIcon.Visibility = Visibility.Visible;
+
+      notifyIcon.TrayPopup = new FancyPopup();
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+      base.OnClosed(e);
+
+      //close the notify icon
+      notifyIcon.Dispose();
+    }
+  }
+}
